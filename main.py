9a973ae2660d9c73fabfe9b8201b5b98a0c98960
Pablosarti7 <paulsarti7@gmail.com>
2025-01-17 13:49:13 -0500

    Adds barcode column to DB table

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from typing import Optional
 from fastapi import FastAPI, HTTPException, Depends, Security, Header  # type: ignore
-from fastapi.security import APIKeyHeader # type: ignore
+from fastapi.security import APIKeyHeader  # type: ignore
 from sqlmodel import Field, SQLModel, create_engine, Session, select  # type: ignore
 import os
 import uvicorn  # type: ignore
 from sqlalchemy.exc import IntegrityError
 
 # Database model
+
+
 class FoodProduct(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(max_length=100)
     ingredients: str = Field(max_length=10000)
     category: Optional[str] = Field(default=None, max_length=50)
+    barcode: Optional[str] = Field(default=None, max_length=50)
 
 
 # Set up database engine
@@ -59,29 +62,33 @@ async def verify_api_key(api_key: str = Security(api_key_header)):
 # Add a product to the database
 @app.post("/add-product")
 def add_product(item: FoodProduct, session: Session = Depends(get_session), api_key: str = Depends(verify_api_key)):
-    
+
     statement = select(FoodProduct).where(FoodProduct.name == item.name)
     results = session.exec(statement)
-    existing_product = results.first()  # Fetches the first matching result or None if no match found.
+    # Fetches the first matching result or None if no match found.
+    existing_product = results.first()
 
     if existing_product:
-        raise HTTPException(status_code=409, detail="Product with the same name already exists.")
-    
+        raise HTTPException(
+            status_code=409, detail="Product with the same name already exists.")
+
     session.add(item)
     try:
         session.commit()
         session.refresh(item)
-        return item # Return the newly added item.
+        return item  # Return the newly added item.
     except Exception as e:
         session.rollback()
-        raise HTTPException(status_code=500, detail="An unexpected error occurred.")
+        raise HTTPException(
+            status_code=500, detail="An unexpected error occurred.")
 
 # Get a specific ingredient
 @app.get("/product/{product_name}")
 def get_ingredient(product_name: str, session: Session = Depends(get_session)):
-    statement = select(FoodProduct).where(FoodProduct.name.contains(product_name))
+    statement = select(FoodProduct).where(
+        FoodProduct.name.contains(product_name))
     results = session.exec(statement).first()
-    
+
     if not results:
         raise HTTPException(status_code=404, detail="Ingredient not found")
     return results
